f712721a7af3e3cb27cff07d5c4c9d88254b0cb7
Shubham7204 <onlinelearner01learn@gmail.com>
2025-02-05 14:57:13 +0530

    Refactor TranslationForm with streaming translation and improved state management

---
+++ client/src/languages.ts
@@ -0,0 +1,13 @@
+export const languages = [
+    { code: 'en', name: 'English' },
+    { code: 'bn', name: 'Bengali' },
+    { code: 'hi', name: 'Hindi' },
+    { code: 'kn', name: 'Kannada' },
+    { code: 'mr', name: 'Marathi' },
+    { code: 'ml', name: 'Malayalam' },
+    { code: 'gu', name: 'Gujarati' },
+    { code: 'pa', name: 'Punjabi' },
+    { code: 'te', name: 'Telugu' },
+    { code: 'ta', name: 'Tamil' },
+  ];
+  
